246138694bffb5595d4e6b1e02d2002e7fd319ab
Daniel Mooney <daniel.mooney180@gmail.com>
2026-02-03 21:55:42 +1100

    improved command interface and Result type returns for forward compatibility

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![no_std]
 
 use embedded_hal::{digital, pwm};
+use core::convert::Infallible;
 
 /// Driver wrapper for one half of an **L298N** Dual Full-Bridge.
 ///
@@ -28,9 +29,9 @@ use embedded_hal::{digital, pwm};
 /// - `C`: timer channel used for the PWM output.
 pub struct L298NHBridge<P1, P2, EN>
 where 
-    P1: digital::OutputPin,
-    P2: digital::OutputPin,
-    EN: pwm::SetDutyCycle,
+    P1: digital::OutputPin<Error = Infallible>,
+    P2: digital::OutputPin<Error = Infallible>,
+    EN: pwm::SetDutyCycle<Error = Infallible>,
 {
     dir1: P1,
     dir2: P2,
@@ -38,67 +39,65 @@ where
     throttle: u16,
 }
 
-/// Command sent to a motor driver.
-///
-/// The motor throttle is set using an `u16`: 
-/// `0u16`      = stopped
-/// `65535u16`  = maximum throttle
-pub enum MotorFunction {
-    /// Set motor throttle in a forward state
-    Forward(u16),
-
-    /// Set motor throttle in a reverse state
-    Reverse(u16),
-
-    /// Set motor to FastMotorStop mode
-    FastMotorStop,
-
-    /// Set motor to FreeRunningMotorStop mode
-    FreeRunningMotorStop,
+/// A `Command` sent to a motor driver
+pub enum Command {
+    Drive { direction: Direction, throttle: u16 },
+    Stop(StopMode),
 }
+
+/// The direction of the H-Bridge
+pub enum Direction { Forward, Reverse }
+
+/// Each `StopMode` variant maps to a stop mode specified in the datasheet:
+/// - Brake -> Fast Motor Stop
+/// - Coast -> Free Running Motor Stop
+pub enum StopMode { Brake, Coast }
 
 impl<P1, P2, EN> L298NHBridge<P1, P2, EN>
 where 
-    P1: digital::OutputPin,
-    P2: digital::OutputPin,
-    EN: pwm::SetDutyCycle,
+    P1: digital::OutputPin<Error = Infallible>,
+    P2: digital::OutputPin<Error = Infallible>,
+    EN: pwm::SetDutyCycle<Error = Infallible>,
 {
 
-    pub fn new(dir1: P1, dir2: P2, enable: EN) -> Self {
+    pub fn new(dir1: P1, dir2: P2, enable: EN) -> Result<Self,Infallible> {
         let mut  handle = Self { dir1, dir2, enable, throttle: 0u16 };
-        handle.enable.set_duty_cycle(0u16).ok();
+        handle.enable.set_duty_cycle(0u16)?;
 
-        handle
+        Ok(handle)
     }
 
-    pub fn set_function(&mut self, function: MotorFunction) {
-        // Set the mode
-        match function {
-             MotorFunction::Forward(_) => self.forward(),
-             MotorFunction::Reverse(_) => self.reverse(),
-             MotorFunction::FreeRunningMotorStop => self.free_running_motor_stop(),
-             MotorFunction::FastMotorStop => self.fast_motor_stop(),
-        } 
-        
-        // Set duty cycle
-        let throttle = match function {
-            MotorFunction::Forward ( throttle ) | MotorFunction::Reverse ( throttle ) => throttle,
-            MotorFunction::FastMotorStop => u16::MAX,
-            MotorFunction::FreeRunningMotorStop => 0u16,
-        };
+    pub fn set(&mut self, cmd: Command) -> Result<(), Infallible> {
+        match cmd {
+            Command::Drive { direction, throttle } => {
+                match direction {
+                    Direction::Forward => self.forward()?,
+                    Direction::Reverse => self.reverse()?,
+                }
 
-        self.set_throttle(throttle);
+                self.set_throttle(throttle)?;
+            },
+            Command::Stop(stop_mode) => {
+                match stop_mode {
+                    StopMode::Brake => self.fast_motor_stop()?,
+                    StopMode::Coast => self.free_running_motor_stop()?,
+                }
+            }
+        }
+        Ok(())
     }
 
     pub fn get_throttle(&self) -> u16 {
         self.throttle
     }
 
-    fn set_throttle(&mut self, throttle: u16) {
+    fn set_throttle(&mut self, throttle: u16) -> Result<(), Infallible> {
         self.throttle = throttle;
 
         let duty = self.duty_from_fullscale(throttle);
-        self.enable.set_duty_cycle(duty).ok();
+        self.enable.set_duty_cycle(duty)?;
+
+        Ok(())
     }
 
     fn duty_from_fullscale(&self, throttle: u16) -> u16 {
@@ -109,25 +108,33 @@ where
     }
     
     /// Sets the L298 into forward mode
-    fn forward(&mut self) {
-        self.dir1.set_high().ok();
-        self.dir2.set_low().ok();
+    fn forward(&mut self) -> Result<(), Infallible> {
+        self.dir1.set_high()?;
+        self.dir2.set_low()?;
+
+        Ok(())
     }
 
     /// Sets the L298 into reverse mode
-    fn reverse(&mut self) {
-        self.dir1.set_low().ok();
-        self.dir2.set_high().ok();
+    fn reverse(&mut self) -> Result<(), Infallible> {
+        self.dir1.set_low()?;
+        self.dir2.set_high()?;
+
+        Ok(())
     }
 
     /// Sets the L298 into fast motor stop mode
-    fn fast_motor_stop(&mut self) {
-        self.dir1.set_high().ok();
-        self.dir2.set_high().ok();
+    fn fast_motor_stop(&mut self) -> Result<(), Infallible> {
+        self.dir1.set_high()?;
+        self.dir2.set_high()?;
+        self.set_throttle(u16::MAX)?;
+
+        Ok(())
     }
 
     /// Sets the L298 into free running motor stop mode
-    fn free_running_motor_stop(&mut self) {
-        self.set_throttle(0u16);
+    fn free_running_motor_stop(&mut self) -> Result<(), Infallible> {
+        self.set_throttle(0u16)?;
+        Ok(())
     }
 }
